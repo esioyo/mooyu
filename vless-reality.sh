5d1c37af57db2b37bc73eb101b299561a00f8a6b
MooYu <123295965+esioyo@users.noreply.github.com>
2026-05-21 20:26:56 +0800

    Update vless-reality.sh

--- vless-reality.sh
+++ vless-reality.sh
@@ -240,8 +240,8 @@ fi
 # 目标网站
 if [[ -z $domain ]]; then
   echo -e "请输入一个 ${magenta}伪装域名${none}"
-  read -p "(默认: www.icloud.com): " domain
-  [ -z "$domain" ] && domain="www.icloud.com"
+  read -p "(默认: bing.com): " domain
+  [ -z "$domain" ] && domain="bing.com"
 
   echo
   echo
